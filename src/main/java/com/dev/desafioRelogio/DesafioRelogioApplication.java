c0befe74ea15305a49246cb498257f862dcebab4
JoaoVitorLima <joaovitorlo@outlook.com>
2026-09-22 15:25:10 -0300

    Real First Commit

---
+++ src/main/java/com/dev/desafioRelogio/DesafioRelogioApplication.java
@@ -0,0 +1,13 @@
+package com.dev.desafioRelogio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRelogioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRelogioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/desafioRelogio/DesafioRelogioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.desafioRelogio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRelogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
